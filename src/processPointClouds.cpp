f9a2a67702729be746e6a70d0130d89a5d71be6c
Peter LeVasseur <plevasseur@gmail.com>
2019-12-30 14:45:03 -0500

    Added code that will remove points reflected from top of ego vehicle.

--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -44,6 +44,24 @@ typename pcl::PointCloud<PointT>::Ptr ProcessPointClouds<PointT>::FilterCloud(ty
     roi.setMax(maxPoint);
     roi.filter(*cloud_roi);
 
+    std::vector<int> indices;
+
+    typename pcl::CropBox<PointT> roof;
+    roof.setInputCloud(cloud_filtered);
+    roof.setMin(Eigen::Vector4f(-1.5, -1.7, -1, 1));
+    roof.setMax(Eigen::Vector4f( 2.6,  1.7, -4, 1));
+    roi.filter(indices);
+
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    for (int point : indices)
+    { inliers->indices.push_back(point); }
+
+    pcl::ExtractIndices<PointT> extract;
+    extract.setInputCloud(cloud_roi);
+    extract.setIndices(inliers);
+    extract.setNegative(true);
+    extract.filter(*cloud_roi);
+
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "filtering took " << elapsedTime.count() << " milliseconds" << std::endl;
